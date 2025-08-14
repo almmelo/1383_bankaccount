e6399a8882e14c490ce3c926340518ca6f588b0f
almmelo <224877868+almmelo@users.noreply.github.com>
2025-08-14 19:19:23 -0300

    Add BankTransation

--- src/main/java/ada/tech/lms/screen/TransactionOptions.java
+++ src/main/java/ada/tech/lms/screen/TransactionOptions.java
@@ -4,4 +4,14 @@ public enum TransactionOptions {
 
     WITHDRAW (1, "Saque"),
     DEPOSIT (2, "Depósito");
+
+    private int opTransaction;
+    private String descTransaction;
+
+    TransactionOptions(int opTransaction, String descTransaction){
+
+        this.opTransaction =  opTransaction;
+        this.descTransaction = descTransaction;
+
+    }
 }
