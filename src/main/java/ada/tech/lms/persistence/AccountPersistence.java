d73c533979c735508f6aefef64ab21ae2045916a
almmelo <224877868+almmelo@users.noreply.github.com>
2025-08-14 22:10:49 -0300

    Refatorando persistencia

--- src/main/java/ada/tech/lms/persistence/AccountPersistence.java
+++ src/main/java/ada/tech/lms/persistence/AccountPersistence.java
@@ -2,29 +2,27 @@ package ada.tech.lms.persistence;
 
 import ada.tech.lms.domain.BankAccount;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 class AccountPersistence {
-    private Path filePath = new Path();
+    private Path filePath = getPath();
 
 
     public boolean openAccountFile(BankAccount account) throws IOException {
-        String filePath = "src/main/resources/conta_" + account.getOwner().getCpf() + ".txt";
-        File file = new File(filePath);
 
-        if (!file.exists()) {
-            file.createNewFile();
-
-
-        }
 
         return true;
 
     }
 
-    public getPath(){
+    private Path getPath(BankAccount account) throws IOException {
+        Path filePath = Paths.get("src","main","resources","conta_" + account.getOwner().getCpf() + ".txt").toAbsolutePath();
+
+        if (!filePath.toFile().exists()) {
+            filePath.toFile().createNewFile();
+        }
 
     }
 
---
+++ src/main/java/ada/tech/lms/persistence/UserPersistence.java
@@ -0,0 +1,8 @@
+package ada.tech.lms.persistence;
+
+import java.nio.file.Paths;
+
+public class UserPersistence {
+
+    private Paths filePath = "scr/main/respources/user_"
+}
